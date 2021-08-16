5f652b7a7b0d343c838124b6c22f0c4222225c36
Syehan <muhammad.syehan@gmail.com>
2021-08-16 17:07:23 +0700

    Enhance DeviceTrait, support behavior class

--- traits/DeviceTrait.php
+++ traits/DeviceTrait.php
@@ -1,39 +1,26 @@
 <?php namespace Octobro\Devices\Traits;
 
 use Cache;
-
+use RainLab\User\Models\User;
 trait DeviceTrait
 {
-
-    /**
-     * Boot the Device trait for a model
-     *
-     * @return void
-     */
-    public static function bootDeviceTrait()
-    {
-        static::extend(function($model) {
-            $model->bindEvent('model.beforeSave', function() use ($model) {
-                $model->deviceTraitBeforeSave();
-            });
-        });
-    }
-
-    public function deviceTraitBeforeSave()
-    {
-
-    }
-
     public function getDeviceTokensAttribute() : array
     {
+        // First, define property $model in construct first if it's traits Behavior Class
+        if($this instanceof User){
+            $model = $this;
+        }else{
+            $model = $this->model;
+        }
+
         // By Caching first
-        $cacheName = sprintf('%s_token_devices', $this->id);
+        $cacheName = sprintf('%s_token_devices', $model->id);
         if(Cache::has($cacheName)){
             return (array) Cache::get($cacheName);
         }
 
         // Make Sure the Relation name is "devices"
-        return (array) $this->devices()
+        return (array) $model->devices()
         ->select('push_token')
         ->remember(5)
         ->pluck('push_token')
